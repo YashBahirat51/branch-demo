66b0561ee77d18b9edfa8bbdcbef0696c1b43bdc
yash bahirat <yashbahirat51@gmail.com>
2024-05-30 23:10:52 +0530

    commit by branch2

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ class HelloWorld
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world");
-		System.out.printnl("line by branch1");)
+		System.out.printnl("line by branch1");
+		System.out.println("line by branch2");
 	}
 }
